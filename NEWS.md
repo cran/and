e09e97b870b555f4b24dea01f148b8d41d93db22
Alexander Rossell Hayes <alexander@rossellhayes.com>
2022-08-17 16:50:02 +0000

    version 0.1.2

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,7 @@
+# and 0.1.2
+
+* Add support for Unix machines without UTF-8 support.
+
 # and 0.1.1
 
 * `set_language()` can now unset the `LANGUAGE` environment variable if its input is an empty string (`""`) or has length 0 (e.g. `NULL`).
--- R/utils.R
+++ R/utils.R
@@ -2,28 +2,6 @@ tag <- function(...) {
   return("")
 }
 
-# @staticimports pkg:staticimports
-#  is_windows
-
-# @staticimports pkg:stringstatic
-#  str_replace_all
-
-normalize <- function(x) {
-  if (rlang::is_installed("stringi")) {
-    return(
-      stringi::stri_trans_tolower(
-        stringi::stri_trans_general(x, "Any-Latin; Latin-ASCII")
-      )
-    )
-  }
-
-  if (!is_windows()) {
-    ascii <- iconv(x, to = "ASCII//TRANSLIT")
-  }
-
-  if (is_windows() || is.na(ascii)) {
-    ascii <- str_replace_all(x, accented_vowels_regex)
-  }
-
-  tolower(ascii)
+release_questions <- function() {
+  "Did you run `urlchecker::url_check()`?"
 }
